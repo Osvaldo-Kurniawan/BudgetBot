651a04d0278098bddfbd746a6b3ced473d7970cf
Osvaldo <osvaldo.giok@gmail.com>
2026-03-18 11:04:56 +0700

    chore: move credential to .env

--- googleSheet.js
+++ googleSheet.js
@@ -2,8 +2,23 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 const { JWT } = require("google-auth-library");
 const dayjs = require("dayjs");
 const customParseFormat = require("dayjs/plugin/customParseFormat");
-const serviceAccount = require("./service_account.json");
+require("dotenv").config();
 dayjs.extend(customParseFormat);
+
+// Load service account from .env
+const serviceAccount = {
+  type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.GOOGLE_SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+};
 
 function mapRow(row) {
   return {
